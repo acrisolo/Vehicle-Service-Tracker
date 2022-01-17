c6816bf74c9901b41719be27ff8cc9192050174a
acrisolo <acrisolo@samford.edu>
2022-01-17 17:12:38 -0600

    Initial Commit

---
+++ Vehicle Service Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vehicle Service Tracker
+//
+//  Created by Alan Crisologo on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
