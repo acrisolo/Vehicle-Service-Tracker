defbd7e54cef11935d93365c70f07e5ad8d23d60
acrisolo <acrisolo@samford.edu>
2022-02-02 10:42:44 -0600

    Added a test template of inputting user Vehicle Data. Work in progress

--- Vehicle Service Tracker/ViewController.swift
+++ Vehicle Service Tracker/ViewController.swift
@@ -9,22 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vYear: UITextField!
+    
+    @IBOutlet weak var vMakeModel: UITextField!
+    
+    @IBOutlet weak var vPurchaseDate: UITextField!
+    
+    @IBOutlet weak var vTotalMiles: UITextField!
+    
+    @IBOutlet weak var vLastService: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFields()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func AddVehicle(_ sender: UIButton) {
     
-        let vc = UIViewController()
-        vc.view.backgroundColor = .red
-        
-        navigationController?.pushViewController(vc, animated: true)
-    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(){
+        print("Handle tap was called")
+        view.endEditing(true)
+    }
+    
+    private func configureTextFields(){
+        //vYear.delegate = self
+        //vMakeModel.delegate = self
+        //vPurchaseDate.delegate = self
+        //vTotalMiles.delegate = self
+        //vLastService.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        vYear.resignFirstResponder()
+        vTotalMiles.resignFirstResponder()
+    }
+    
+    @IBAction func enterTapped(_ sender: Any) {
+        textView.text = "Year: \(vYear.text!)\nMake and Model: \(vMakeModel.text!)\nPurchase Date: \(vPurchaseDate.text!)\nTotal Miles: \(vTotalMiles.text!)\nLast Service Date: \(vLastService.text!)"
+        view.endEditing(true)
+    }
+    
+}
 
+extension ViewController : UITextFieldDelegate {
     
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
